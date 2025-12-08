7dcdc989469cde18fd34dae04e419d9076ac2e78
ande-y <andy.cheng0814@gmail.com>
2025-12-07 20:59:28 -0500

    progress 2

--- portfolio.js
+++ portfolio.js
@@ -1,6 +1,23 @@
-// window.alert(`hello`);
+const text = document.getElementById("occupationText");
 
-let x = 1;
-++x;
+const timeInterval = 3500;
+setInterval(textVanish, timeInterval);
 
-console.log(x);
+let index = 0;
+const occupations = ["Computer Science Major",
+                     "Video Game Developer",
+                     "Illustrator & Graphic Artist",
+                     "Full Stack Web Developer"];
+
+function textVanish(){
+    text.classList.remove("appearing");
+    text.classList.add("fading");
+    console.log("yes");
+
+    setTimeout(textAppear, 500);
+}
+function textAppear(){
+    index = (index + 1) % occupations.length;
+    text.textContent = occupations[index];
+    text.classList.replace("fading", "appearing");
+}
