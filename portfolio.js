6e022907777606ed18d893dac73ac45fda0fa09f
ande-y <andy.cheng0814@gmail.com>
2026-02-19 14:07:51 -0500

    navbar working

--- portfolio.js
+++ portfolio.js
@@ -15,6 +15,18 @@ function textChange(){
     text.textContent = occupations[iA];
 }
 
+// NAVBAR BUTTONS SCROLL
+
+function scrollPage(destName){
+    let dest = document.getElementById(destName);
+    let rect = dest.getBoundingClientRect();
+    window.scrollTo({
+        left: 0, 
+        top: rect.top + window.scrollY, 
+        behavior: "smooth"
+    });
+}
+
 // GALLERY ANIMATION PLAYERS
 
 const gall = document.getElementsByClassName("gallery");
@@ -24,5 +36,5 @@ setInterval(changeImg, 6000);
 let iB = 0;
 
 function changeImg(){
-    console.log("yuh")
+    // console.log("yuh")
 }
--- test/test.js
+++ test/test.js
@@ -6,3 +6,14 @@ function playAnim(){
     text.classList.toggle("slideAnim");
     console.log("yes");
 } 
+
+function saySomething() {
+    var greeting = "Hello";
+    {
+        greeting = "Howdy";  // error comes from here
+        // let greeting = "Hi";
+        console.log(greeting);
+    }
+}
+
+saySomething();
